07278ed32088f721073f22ffa2a45981d7c75243
Barney W <B.Wilton1993@gmail.com>
2021-09-13 13:52:18 +0100

    Upwards thrust now applies

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float thrustForce = 20f;
+
+    public Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust() 
+    {
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            Debug.Log("Pressed SPACE - Thrust applied");
+            rb.AddRelativeForce(Vector3.up * thrustForce);
+        }
+    }
+
+    void ProcessRotation() 
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            Debug.Log("Pressed LEFT - Rocket turned left");
+        } 
+        else if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+            Debug.Log("Pressed RIGHT - Rocket turned right");
+        }
     }
 }
