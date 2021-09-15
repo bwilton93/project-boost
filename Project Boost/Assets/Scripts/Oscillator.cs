ed0319c8a73f693562b4cf9ec3cdee7cbe9c493c
Barney W <B.Wilton1993@gmail.com>
2021-09-15 11:27:17 +0100

    Created oscillator for pistons in first lab level

---
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour {
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 2f;
+
+    void Start() {
+        startingPosition = transform.position;
+        Debug.Log(startingPosition);
+    }
+
+    void Update() {
+        float cycles = Time.time / period; // Constantly grows over time
+
+        const float tau = Mathf.PI * 2; // Constant value of 6.283
+        float rawSinWave = Mathf.Sin(tau * cycles); // Going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // Recalculated sin wave now goes between 0 and 1
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
